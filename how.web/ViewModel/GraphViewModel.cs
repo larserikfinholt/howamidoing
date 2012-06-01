5b0ce7c974d89ddb14ffa0637965e045682a3da2
Lars Erik Finholt <lars.erik.finholt@gmail.com>
2012-06-01 22:31:24 +0200

    kg

---
+++ how.web/ViewModel/GraphViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace how.web.ViewModel
+{
+    public class GraphViewModel
+    {
+        public List<GraphPoint> Points { get; set; }
+    }
+    public class GraphPoint
+    {
+        public decimal x { get; set; }
+        public decimal y { get; set; }
+        public DateTime Time { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
